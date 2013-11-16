c694d8ac419e55971bf25d834445465e58d00504
Andrii Lashchenko <andrew.lashchenko@gmail.com>
2013-11-16 13:52:54 +0200

    Replace def main to scala.App http://www.scala-lang.org/api/current/index.html#scala.App

--- examples/import/src/main/scala/io/prediction/samples/ScalaImport.scala
+++ examples/import/src/main/scala/io/prediction/samples/ScalaImport.scala
@@ -16,7 +16,7 @@ case class ScalaImportConfig(
   *
   * @author The PredictionIO Team
   */
-object ScalaImport {
+object ScalaImport extends App {
   /** Control variables shared with the consumer thread */
   var allSent = false
   var allDone = false
@@ -25,103 +25,101 @@ object ScalaImport {
   /** Maximum number of requests allowed in memory */
   val MaxRequests = 1000
 
-  def main(args: Array[String]) {
-    val parser = new scopt.OptionParser[ScalaImportConfig]("scala-import") {
-      head("PredictionIO Data Import Tool in Scala", "0.1.0")
-      help("help") text("prints this usage text")
-      arg[String]("<app key>") action { (x, c) =>
-        c.copy(appkey = x)
-      } text("app key")
-      arg[String]("<input file>") action { (x, c) =>
-      	c.copy(inputFile = x)
-      } text("u.data file from MovieLens 100k data set")
-      opt[String]("apiurl") action { (x, c) =>
-        c.copy(apiurl = x)
-      } text("API URL (default: http://localhost:8000)")
-    }
+  val parser = new scopt.OptionParser[ScalaImportConfig]("scala-import") {
+    head("PredictionIO Data Import Tool in Scala", "0.1.0")
+    help("help") text("prints this usage text")
+    arg[String]("<app key>") action { (x, c) =>
+      c.copy(appkey = x)
+    } text("app key")
+    arg[String]("<input file>") action { (x, c) =>
+      c.copy(inputFile = x)
+    } text("u.data file from MovieLens 100k data set")
+    opt[String]("apiurl") action { (x, c) =>
+      c.copy(apiurl = x)
+    } text("API URL (default: http://localhost:8000)")
+  }
 
-    parser.parse(args, ScalaImportConfig()) map { scalaImportConfig =>
-      /** Create a client with an app key */
-      val client = new Client(scalaImportConfig.appkey)
+  parser.parse(args, ScalaImportConfig()) map { scalaImportConfig =>
+    /** Create a client with an app key */
+    val client = new Client(scalaImportConfig.appkey)
 
-      /** Data structure */
-      val uids = collection.mutable.Set[String]()
-      val iids = collection.mutable.Set[String]()
-      val q = new LinkedBlockingQueue[FutureAPIResponse]()
+    /** Data structure */
+    val uids = collection.mutable.Set[String]()
+    val iids = collection.mutable.Set[String]()
+    val q = new LinkedBlockingQueue[FutureAPIResponse]()
 
-      /** Start the consumer thread to synchronize sent requests */
-      FutureExaminer(q)
+    /** Start the consumer thread to synchronize sent requests */
+    FutureExaminer(q)
 
-      /** Read input MovieLens data and send requests to API */
-      try {
-        /** Get API status */
-        println(client.getStatus())
+    /** Read input MovieLens data and send requests to API */
+    try {
+      /** Get API status */
+      println(client.getStatus())
 
-        scala.io.Source.fromFile(scalaImportConfig.inputFile).getLines().foreach { line =>
-          /** Throttling */
-          while (q.size() > MaxRequests) {
-            Thread.sleep(1)
-          }
-
-          /** The 1st field is User ID, the 2nd field is Item ID, and the 3rd field is rating */
-          val (uid :: iid :: rate :: _) = line.split('\t').toList
-
-          /** Save User IDs and Item IDs for adding later */
-          uids.add(uid)
-          iids.add(iid)
-
-          /** Identify as current User ID */
-          client.identify(uid)
-
-          /** Create all types of actions for testing purpose
-            * Push async handler to queue for synchronization
-            */
-          q.put(client.userActionItemAsFuture(client.getUserActionItemRequestBuilder("view", iid)))
-          q.put(client.userActionItemAsFuture(client.getUserActionItemRequestBuilder("like", iid)))
-          q.put(client.userActionItemAsFuture(client.getUserActionItemRequestBuilder("dislike", iid)))
-          q.put(client.userActionItemAsFuture(client.getUserActionItemRequestBuilder("conversion", iid)))
-          q.put(client.userActionItemAsFuture(client.getUserActionItemRequestBuilder("rate", iid).rate(rate.toInt)))
-
-          /** Increase counter */
-          totalSent += 5
-
-          /* Print status per 2000 requests */
-          if (totalSent % 2000 == 0) {
-            println(s"Sent $totalSent requests so far")
-          }
+      scala.io.Source.fromFile(scalaImportConfig.inputFile).getLines().foreach { line =>
+        /** Throttling */
+        while (q.size() > MaxRequests) {
+          Thread.sleep(1)
         }
 
-        /** Add User and Item IDs asynchronously */
-        println(s"Sending ${uids.size} create User ID requests")
-        uids.foreach { uid =>
-          q.put(client.createUserAsFuture(client.getCreateUserRequestBuilder(uid)))
-          totalSent += 1
-        }
+        /** The 1st field is User ID, the 2nd field is Item ID, and the 3rd field is rating */
+        val (uid :: iid :: rate :: _) = line.split('\t').toList
 
-        println(s"Sending ${iids.size} create Item ID requests")
-        val itypes = Array("movies")
-        iids.foreach { iid =>
-          q.put(
-            client.createItemAsFuture(
-              client.getCreateItemRequestBuilder(iid, itypes)
-                .attribute("url", s"http://localhost/${iid}.html")
-                .attribute("startT", "ignored")))
-          totalSent += 1
-        }
+        /** Save User IDs and Item IDs for adding later */
+        uids.add(uid)
+        iids.add(iid)
 
-        /** Tell the future examiner that everything has been sent */
-        allSent = true
-      } catch {
-        case e: Exception => println(s"Error: ${e.getMessage()}")
+        /** Identify as current User ID */
+        client.identify(uid)
+
+        /** Create all types of actions for testing purpose
+          * Push async handler to queue for synchronization
+          */
+        q.put(client.userActionItemAsFuture(client.getUserActionItemRequestBuilder("view", iid)))
+        q.put(client.userActionItemAsFuture(client.getUserActionItemRequestBuilder("like", iid)))
+        q.put(client.userActionItemAsFuture(client.getUserActionItemRequestBuilder("dislike", iid)))
+        q.put(client.userActionItemAsFuture(client.getUserActionItemRequestBuilder("conversion", iid)))
+        q.put(client.userActionItemAsFuture(client.getUserActionItemRequestBuilder("rate", iid).rate(rate.toInt)))
+
+        /** Increase counter */
+        totalSent += 5
+
+        /* Print status per 2000 requests */
+        if (totalSent % 2000 == 0) {
+          println(s"Sent $totalSent requests so far")
+        }
       }
 
-      /** Wait for synchronization */
-      while (!allDone) {
-        Thread.sleep(1)
+      /** Add User and Item IDs asynchronously */
+      println(s"Sending ${uids.size} create User ID requests")
+      uids.foreach { uid =>
+        q.put(client.createUserAsFuture(client.getCreateUserRequestBuilder(uid)))
+        totalSent += 1
       }
 
-      client.close()
+      println(s"Sending ${iids.size} create Item ID requests")
+      val itypes = Array("movies")
+      iids.foreach { iid =>
+        q.put(
+          client.createItemAsFuture(
+            client.getCreateItemRequestBuilder(iid, itypes)
+              .attribute("url", s"http://localhost/${iid}.html")
+              .attribute("startT", "ignored")))
+        totalSent += 1
+      }
+
+      /** Tell the future examiner that everything has been sent */
+      allSent = true
+    } catch {
+      case e: Exception => println(s"Error: ${e.getMessage()}")
     }
+
+    /** Wait for synchronization */
+    while (!allDone) {
+      Thread.sleep(1)
+    }
+
+    client.close()
   }
 
   /** A simple consumer examining the HTTP request result code */
